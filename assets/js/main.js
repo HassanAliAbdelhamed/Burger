9a5cfc3e5f7abf33d5196120ebf7efcb68d564ca
Elhassan Ali Abdelhamid <108933298+HassanAliAbdelhamed@users.noreply.github.com>
2023-12-16 17:55:53 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,15 +20,28 @@ total.onclick = ()=>{
     let up = document.querySelector('footer .imageBox .up img');
     let middle = document.querySelectorAll('footer .imageBox .imageInsinde img');
     let down = document.querySelector('footer .imageBox .down img');
-    let cheese = document.getElementById('cheese');
-    let mayo = document.getElementById('mayo');
     
+    let cheese = document.querySelectorAll('#cheese');
+    let mayo = document.querySelectorAll('#mayo');
+    let cucumber = document.querySelectorAll('#cucumber');
+
     up.style.marginBottom ="-25px";
     middle.forEach(ele => {
         ele.style.marginBottom ="-27px";
-    });   
-    cheese.style="margin-bottom : -45px";
-    mayo.style.marginBottom = '-35px';
+    });  
+
+    cheese.forEach(ele => {
+        ele.style="margin-bottom : -45px";
+    });
+
+    mayo.forEach(ele => {
+        ele.style="margin-bottom : -35px";
+    });
+
+    cucumber.forEach(ele => {
+        ele.style="margin-top : -5px ;margin-bottom : -27px;transform: rotate(-1deg);";
+    });
+
     down.style.marginBottom ="-13px";
     
 }
